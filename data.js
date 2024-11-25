52ab090a6083d899768df54fda8d45245743e7de
oickle <oycell@aol.com>
2024-11-25 15:09:01 -0500

    Add comment

--- data.js
+++ data.js
@@ -8,6 +8,7 @@ async function fetchCrimeData(url) {
     }
 }
 
+// Fetches data from json
 async function setCrime(index) {
     var data = await fetchCrimeData("https://data.cityofgainesville.org/resource/gvua-xt9q.json");
     var crime = data[index]
