5db2d1f2451e03a0fa38d8b694c72983b5ff31e5
Krithika Kondapalli <krithika.kondapalli@gmail.com>
2024-11-25 21:34:24 -0500

    Update min heap

--- C++/MinHeap.h
+++ C++/MinHeap.h
@@ -9,9 +9,9 @@ class MinHeap
 public:
 vector<Crime> minHeap;
 void insert(Crime crime);
-void extractMin();
+Crime extractMin();
 void heapifyUp(int childindex);
-void heapifyDown();
+void heapifyDown(int index);
 
 };
 
@@ -28,10 +28,46 @@ void MinHeap::heapifyUp(int childIndex){
     int parentIndex = (childIndex - 1) / 2; // this already does floor
 
     // if child is less than parent, swap elements
-    // if(childIndex > 0 && minHeap[parentIndex].distance < minHeap[childIndex].distance){
-    //     swap(minHeap[childIndex], minHeap[parentIndex]);
+    if(childIndex > 0 && minHeap[parentIndex].distance < minHeap[childIndex].distance){
+        swap(minHeap[childIndex], minHeap[parentIndex]);
 
-    //     // keep heapifying up recursively
-    //     heapifyUp(parentIndex);
-    // }
+        // keep heapifying up recursively
+        heapifyUp(parentIndex);
+    }
+}
+
+Crime MinHeap::extractMin(){
+    Crime minElement = minHeap[0];
+
+    // assign root with last element in min heap
+    minHeap[0] = minHeap[minHeap.size()-1];
+
+    // delete last element
+    minHeap.pop_back();
+
+    // heapify down
+    heapifyDown(0);
+
+    return minElement;
+}
+
+void MinHeap::heapifyDown(int index){
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+    int smallestIndex = index;
+
+    // check if left or right child are smaller
+    if(left < minHeap.size() && minHeap[left].distance < minHeap[index].distance){
+        smallestIndex = left;
+    }
+
+    if(right < minHeap.size() && minHeap[right].distance < minHeap[index].distance){
+        smallestIndex = right;
+    }
+
+    // swap if a smaller element has been found
+    if(smallestIndex != index){
+        swap(minHeap[index], minHeap[smallestIndex]);
+        heapifyDown(smallestIndex);
+    }
 }
