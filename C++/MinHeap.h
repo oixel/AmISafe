6e568a205f82650088dd0bb20e1ae4585b4be265
Krithika Kondapalli <krithika.kondapalli@gmail.com>
2024-11-25 21:07:35 -0500

    Insert Min Heap

--- C++/MinHeap.h
+++ C++/MinHeap.h
@@ -1,9 +1,37 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include "Crime.h"
 using namespace std;
 
 class MinHeap
 {
 public:
+vector<Crime> minHeap;
+void insert(Crime crime);
+void extractMin();
+void heapifyUp(int childindex);
+void heapifyDown();
+
 };
+
+void MinHeap::insert(Crime crime){
+    // insert new element at the end of the vector minHeap.size()-1
+    minHeap.push_back(crime);
+
+    // heapify up 
+    heapifyUp(minHeap.size()-1);
+}
+
+void MinHeap::heapifyUp(int childIndex){
+    // calculate parent
+    int parentIndex = (childIndex - 1) / 2; // this already does floor
+
+    // if child is less than parent, swap elements
+    // if(childIndex > 0 && minHeap[parentIndex].distance < minHeap[childIndex].distance){
+    //     swap(minHeap[childIndex], minHeap[parentIndex]);
+
+    //     // keep heapifying up recursively
+    //     heapifyUp(parentIndex);
+    // }
+}
