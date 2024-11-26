d2b2f42c2c90810cb6f5dd5b03ec469d85bce6fb
oickle <oycell@aol.com>
2024-11-26 12:47:16 -0500

    Make progress

--- C++/HashMap.h
+++ C++/HashMap.h
@@ -1,78 +1,87 @@
-#pragma once
 #include <iostream>
 #include <cmath>
 #include <vector>
 #include "Crime.h"
 using namespace std;
 
-// if we need to visualize: there is a display hash function we can use to make sure everything is being added correctly 
+// if we need to visualize: there is a display hash function we can use to make sure everything is being added correctly
 // https://www.geeksforgeeks.org/hash-table-data-structure/
 
 class HashMap
 {
 public:
-int buckets; 
-float loadFactor = 0.75; // 0.75?
-int entries = 0;
-int resizeCount = 0;
-//vector<Crime> crimes;
+    int buckets;
+    float loadFactor = 0.75; // 0.75?
+    int entries = 0;
+    int resizeCount = 0;
+    // vector<Crime> crimes;
 
+    // creates 2D vector to store the chaining
+    // Key: outer vector holds the distance
+    // Value: holds the other crime objects
+    vector<vector<Crime>> table;
 
-// creates 2D vector to store the chaining 
-// Key: outer vector holds the distance 
-// Value: holds the other crime objects 
-vector<vector<Crime>> table; 
-
-// Constuctor (how are we getting the number of buckets?)
-HashMap(int buckets, vector<Crime> &crimes){
-    this->buckets = buckets;
-    table.resize(buckets);
-    // for(auto crime: crimes){
-    //     insert(crime);
-    // }
-    cout << "Constructor is called" << endl;
-}
-
-//hash function? or do we alr have the keys we just need to input it?
-int hashFunction(float distance){
-    int hashCode = floor(pow(10, resizeCount) * distance);
-    return hashCode;
-}
-
-// resize function
-void resize(){
-    resizeCount++;
-    // resize the buckets: double the number of buckets
-    buckets *= 2;
-    vector<vector<Crime>> tempTable = table;
-    table.clear();
-    table.resize(buckets);
-
-    for(auto row : tempTable){
-        for(int j = 0; j < row.size(); j++){
-            insert(row[j]);
+    // Constuctor (how are we getting the number of buckets?)
+    HashMap(int buckets, vector<Crime> &crimes)
+    {
+        this->buckets = buckets;
+        table.resize(buckets, {});
+        int i = 0;
+        for (auto crime : crimes)
+        {
+            i++;
+            insert(crime);
         }
     }
-}
 
-
-//insert function 
-void insert(Crime crime){
-    
-    int hashCode = hashFunction(crime.distance); // we may need a better hash function to reduce the time complexity
-    table[hashCode].push_back(crime);
-    entries++;
-
-    if (entries/buckets >= loadFactor){
-        resize();
+    // hash function? or do we alr have the keys we just need to input it?
+    unsigned int hashFunction(float distance)
+    {
+        return (unsigned int)(distance * pow(10, resizeCount)) % buckets;
     }
-}
 
-// do we delete or are we just iterating? 
-vector<Crime> getClosestCrimes(float userDefinedDistance){
+    // resize function
+    void resize()
+    {
+        // resize the buckets: double the number of buckets
+        buckets *= 2;
+        vector<vector<Crime>> tempTable = table;
+        table.clear();
+        table.resize(buckets);
 
+        //
+        resizeCount++;
 
-}
+        //
+        entries = 0;
 
+        for (auto row : tempTable)
+        {
+            for (Crime crime : row)
+            {
+                insert(crime);
+            }
+        }
+    }
 
+    //
+    void insert(Crime crime)
+    {
+        int hashCode = hashFunction(crime.distance);
+
+        // cout << hashCode << " in buckets of " << buckets << endl;
+        table[hashCode].push_back(crime);
+
+        // cout << entries << ": " << hashCode << " in bucket size of " << buckets << endl;
+        // cout << "(" << crime.latitude << "," << crime.longitude << ") :: This crime has a distance of " << crime.distance << " @ " << crime.address << endl;
+        entries++;
+
+        if (entries / buckets >= loadFactor)
+            resize();
+    }
+
+    // do we delete or are we just iterating?
+    vector<Crime> getCrimesInRange(float radius)
+    {
+    }
 };
