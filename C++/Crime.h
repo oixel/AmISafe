5cdda833aa16b241b8e7bdbfd3adf8aec135d82a
oickle <oycell@aol.com>
2024-11-25 20:47:42 -0500

    Change structure of Crime class

--- C++/Crime.h
+++ C++/Crime.h
@@ -1,24 +1,28 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 class Crime
 {
 private:
 public:
-    string narrative;
-    string address;
-    float longitude;
-    float latitude;
-    float distance;
+    unordered_map<string, string> data;
+
     // int radius; // we may not need this right now
 
     // Default constructor
-    Crime(string narrative, string address, float longitude, float latitude)
+    Crime(unordered_map<string, string> data)
     {
-        this->narrative = narrative;
-        this->address = address;
-        this->longitude = longitude;
-        this->latitude = latitude;
+        this->data = data;
+    }
+
+    // Displays stored data
+    void display()
+    {
+        cout << "Crime: " << data["incident"] << endl;
+        cout << "Date: " << data["offense_date"] << endl;
+        cout << "Address: " << data["address"] << endl;
+        cout << "(" << data["latitude"] << ", " << data["longitude"] << ")" << endl;
     }
 
     // Updates distance
