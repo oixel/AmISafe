5552c6b512f7b0c2975b74f85e36e2c4fee3e489
oickle <oycell@aol.com>
2024-11-27 13:40:23 -0500

    Implement quadratic probing and getCrimesInRadius()

--- JavaScript/HashTable.js
+++ JavaScript/HashTable.js
@@ -9,8 +9,11 @@ export class HashTable {
         // Key: Hashed key based on distance from current position
         // Value: Crime objects
         this.table = new Array(buckets);
-        this.table.fill([]);
 
+        // Fill array with empty buckets
+        this.table.fill(null);
+
+        // Insert all crimes into hashtable
         crimes.forEach(crime => {
             this.insert(crime);
         });
@@ -27,14 +30,14 @@ export class HashTable {
         this.buckets *= 2;
 
         // Store current table to rehash and reinsert current elements
-        var tempTable = this.table;
+        var tempTable = Array.from(this.table);
 
         // Empty the current table
         this.table.length = 0;
 
         // Fill array with empty arrays
         this.table.length = this.buckets;
-        this.table.fill([]);
+        this.table.fill(null);
 
         // Increment resize count to change hashFunction
         this.resizeCount += 1;
@@ -43,17 +46,29 @@ export class HashTable {
         this.entries = 0;
 
         // Rehash and refill table with old values
-        tempTable.forEach(row => {
-            row.forEach(crime => {
-                this.insert(crime);
-            });
+        tempTable.forEach(crime => {
+            if (crime != null) this.insert(crime);
         });
     }
 
     insert(crime) {
         // Insert crime into hashtable and hashed key index
-        const hashCode = this.hash(crime.distance);
-        this.table[hashCode].push(crime);
+        let hashCode = this.hash(crime.distance);
+
+        // Stores current quadratic for quadratic probing
+        let quad = 1;
+
+        // Loops until an open bucket is found
+        while (this.table[hashCode] !== null) {
+            // Ensures that bucket is in range of total buckets
+            hashCode = (hashCode + quad * quad) % this.buckets;
+
+            // Increment quad on collision for further probing
+            quad++;
+        }
+
+        // Once an open bucket is found, insert crime into it
+        this.table[hashCode] = crime;
 
         // Increment entry count
         this.entries += 1;
@@ -61,5 +76,22 @@ export class HashTable {
         // If load factor has been surpassed, resize the hashtable
         if (this.entries / this.buckets >= this.loadFactor)
             this.resize();
+    }
+
+    // Returns all the crimes within radius of current position
+    getCrimesInRange(radius) {
+        // Stores all the crimes in given radius
+        let crimesInRange = [];
+
+        // Loops through all buckets in table
+        this.table.forEach(crime => {
+            // Only adds crime if bucket is not empty and it is in desired radius
+            if (crime !== null && crime.distance <= radius) {
+                crimesInRange.push(crime);
+            }
+        })
+
+        // Return all the crimes within radius of current position
+        return crimesInRange;
     }
 }
