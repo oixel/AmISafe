1fafeefd88155bfe3378eb1cbc4934c53dd0867f
oickle <oycell@aol.com>
2024-11-26 22:16:08 -0500

    Fix formatting

--- C++/handleData.cpp
+++ C++/handleData.cpp
@@ -96,26 +96,27 @@ int main()
     // cout << endl;
 
     // Get all crimes recorded crimes in Gainesville with adequate data
-    vector<Crime> crimes = getData("Crime_Responses.csv");
+    vector<Crime> crimes = getData("../Crime_Responses.csv");
     // crimes[0].display();
     // cout << crimes.size() << endl;
 
-    // Create min heap and fill it with all crime data
-
     const float RADIUS = 0.5;
+
+    // Create min heap and fill it with all crime data
+    cout << "Min Heap:" << endl;
     auto start = std::chrono::high_resolution_clock::now();
     MinHeap minheap1(crimes);
     cout << "Min heap size: " << minheap1.minHeap.size() << endl;
     cout << "Crimes in " << RADIUS << " mile radius: " << minheap1.getCrimesInRange(RADIUS).size() << endl;
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
-
-    std::cout << "Elapsed time: " << elapsed.count() << " seconds" << std::endl;
+    cout << "Elapsed time: " << elapsed.count() << " seconds" << std::endl;
 
     // Create hash map and fill it with all crime data
+    cout << "\nHash Map:" << endl;
     auto start2 = std::chrono::high_resolution_clock::now();
     HashMap hashTable(207, crimes);
-    cout << hashTable.entries << endl;
+    // cout << hashTable.entries << endl;
     cout << "Crimes in " << RADIUS << " mile radius: " << hashTable.getCrimesInRange(RADIUS).size() << endl;
     auto end2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed2 = end2 - start2;
