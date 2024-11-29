ba36c942ba5ace17c3bc146ae14123abc2430ace
oickle <oycell@aol.com>
2024-11-29 01:00:07 -0500

    Add comments

--- JavaScript/Crime.js
+++ JavaScript/Crime.js
@@ -1,10 +1,9 @@
-// 
-export const CRIME_TYPES = ["Assault", "Arson", "Arrest", "Battery", "Burglary", "Death", "Drug Violation", "Stolen Vehicle", "Theft", "Other"];
+// Stores all filterable crime types
+export const CRIME_TYPES = ["Assault", "Arson", "Arrest", "Battery", "Burglary", "Death", "Drug Violation", "Robbery", "Stolen Vehicle", "Theft", "Other"];
 
 export class Crime {
     constructor(incident, occurence, address, latitude, longitude) {
         this.incident = incident;
-
 
         this.date = occurence.slice(0, 10);  // Get date value from occurence data
         this.year = occurence.slice(6, 10); // Get year value from occurence data
@@ -33,18 +32,21 @@ export class Crime {
         this.distance = Math.sqrt(latMiles ** 2 + longMiles ** 2);
     }
 
+    // Determines crime type out of valid CRIME_TYPES from incident information
     setCrimeType() {
         // Store this.incident into variable to avoid having to write "this." every if statement
         const incident = this.incident;
 
-        // 
+        // Loops through all filterable crime types
         for (var i = 0; i < CRIME_TYPES.length; i++) {
+            // If the incident information contains one of the crime types, assign it to this Crime object
             if (incident.toLowerCase().includes(CRIME_TYPES[i].toLowerCase())) {
                 this.crimeType = CRIME_TYPES[i];
+                break;
             }
         }
 
-        // 
+        // If no matches occur, this incident gets the generic crime type of "Other"
         if (!this.crimeType) this.crimeType = "Other";
     }
 
